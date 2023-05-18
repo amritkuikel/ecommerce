e1801890a7e0c1698aaf8d0a25dc467600a75408
amritkuikel <amritkuikel5689@gmail.com>
2023-05-19 01:28:09 +0545

    mui

--- clientside/src/pages/Login.jsx
+++ clientside/src/pages/Login.jsx
@@ -1,9 +1,10 @@
-import { Link ,useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
+import { TextField, Typography, Container, Button ,Link as Linkm } from "@mui/material";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const submitHandler = (e) => {
@@ -12,45 +13,66 @@ const Login = () => {
       email: email,
       password: password,
     };
-    axios.post('/auth/login',userData).then(
-        (log)=>{
-            if(log.data.success===true){
-                localStorage.setItem("data",log.data.token)
-                alert(log.data.msg)
-                navigate('/dashboard')
-            }
-            else{
-                alert(log.data.msg)
-            }
-        }
-    );
+    axios.post("/auth/login", userData).then((log) => {
+      if (log.data.success === true) {
+        localStorage.setItem("data", log.data.token);
+        alert(log.data.msg);
+        navigate("/dashboard");
+      } else {
+        alert(log.data.msg);
+      }
+    });
   };
   return (
-    <div>
-      <div>Login</div>
-      <form onSubmit={submitHandler}>
-        <label>email</label>
-        <br />
-        <input
-          type="text"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <br />
-        <label>password</label>
-        <br />
-        <input
-          type="text"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <br />
-        <button>submit</button>
-        <br />
-        <Link to={"/register"}>not registered?click here to register</Link>
-      </form>
+    <div className="login-container">
+      <Container
+        maxWidth="sm"
+        sx={{
+          backgroundColor: "#E8AA42",
+          padding: 5,
+          borderRadius: 5,
+          border: 5,
+          borderColor: "#E8AA42",
+        }}
+      >
+        <Typography align="center" variant="h3" marginBottom={3}>
+          LOGIN
+        </Typography>
+        <form onSubmit={submitHandler}>
+          <Typography variant="h6" >Enter Your Email Below:</Typography>
+          <br />
+          <TextField
+            color="secondary"
+            size="small"
+            label="Input Email Here"
+            placeholder="your_mail@example.com"
+            variant="outlined"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <br />
+          <Typography variant="h6" marginTop={3}>Enter Your Password:</Typography>
+          <br />
+          <TextField
+            color="secondary"
+            size="small"
+            label="Input Password Here"
+            placeholder="your_mail@example.com"
+            variant="outlined"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <br />
+          <Button variant="contained" color="secondary" sx={{ mt: 3 ,mb:3 }}>
+            SUBMIT
+          </Button>
+          <br />
+            
+          <Link to={"/register"} ><Linkm  underline="none" color='primary'>not registered?click here to register</Linkm></Link>
+        </form>
+      </Container>
     </div>
   );
 };
